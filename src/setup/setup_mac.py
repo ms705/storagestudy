5ac7ec47b7ddd295b906d6fb6669decf7bebc19b
malte <malte@a513dc67-2daa-4240-96f3-f69e19499a29>
2010-07-26 15:56:03 +0000

    Fixed PyQt module name
    git-svn-id: svn+ssh://ganymede.cl.cam.ac.uk/mnt/dev/homedirsurvey@9 a513dc67-2daa-4240-96f3-f69e19499a29

--- src/setup/setup_mac.py
+++ src/setup/setup_mac.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
    app=["src/main.py"],
    options={"py2app":
-      {"argv_emulation": True, "includes": ["sip", "PyQt4._qt"]}
+      {"argv_emulation": True, "includes": ["sip", "PyQt4"]}
    },
    setup_requires=["py2app"]) 
 
