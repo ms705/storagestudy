e0123b78e0afe4ad8477e153ccaad582aac03114
malte <malte@a513dc67-2daa-4240-96f3-f69e19499a29>
2010-07-30 12:15:58 +0000

    Added file to bootstrap the PHP side of things
    git-svn-id: svn+ssh://ganymede.cl.cam.ac.uk/mnt/dev/homedirsurvey@19 a513dc67-2daa-4240-96f3-f69e19499a29

---
+++ src/webui/index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Created on 30 Jul 2010
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+
+// Code to test POST submission of data
+foreach ($_POST as $k => $p) {
+	echo "received item: " . $k;
+} 
+
+?>
